9f2eb7f128a92d4e3a23dcf84e38f3c6df5dbc94
shengxingyue <shengxingyue@souche.com>
2018-01-08 11:24:29 +0800

    使用 lombok 的 @slf4j

--- src/main/java/org/sheng/logger/Main.java
+++ src/main/java/org/sheng/logger/Main.java
@@ -1,15 +1,18 @@
 package org.sheng.logger;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author shengxingyue, created on 2018/1/8
  */
+
+/**
+ * 当使用 lombok 的时候需要带上具体依赖包，@slf4j 只是动态加上了获取日志对象的代码，具体依赖 lombok 并没有带上
+ */
+@Slf4j
 public class Main {
     public static void main(String[] args) {
-        Logger logger = LoggerFactory.getLogger(Main.class);
-        logger.info("hello");
-        logger.warn("world");
+        log.info("hello");
+        log.warn("world");
     }
 }
